a481b92eb356117b0adec085d722c4a76083abfd
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2017-11-12 00:37:09 +0100

    Add simple exponential operator parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import * as babylon from 'babylon';
+import * as types from 'babel-types'
+import traverse from 'babel-traverse';
+import generate from 'babel-generator';
 
 const code = `function square(x) {
-  return x * x;
+  return x ** 2;
 }`;
 
+const expVisitor = {
+  BinaryExpression(path) {
+    if (types.isBinaryExpression(path.node, { operator: "**" })) {
+      path.replaceWith(
+        types.callExpression(
+          types.memberExpression(types.identifier("Math"), types.identifier("pow")),
+          [path.node.left, path.node.right]
+        )
+      );
+    }
+  }
+};
+
 const parsed = babylon.parse(code);
-console.log(parsed);
-console.log('abc');
+traverse(parsed, expVisitor);
+const generated = generate(parsed);
+
+console.log(generated.code);
